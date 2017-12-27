e2329fb9c5f379c52108f462f0618af0e35a5b43
vtpetrov <veselin.t.petrov@gmail.com>
2017-12-27 21:53:41 +0200

    template added

--- src/main/java/Template.java
+++ src/main/java/Template.java
@@ -1,43 +1,42 @@
-package day18;
-
 import java.util.Date;
 
 import static helper.InputLoader.closeInput;
 import static helper.InputLoader.loadInput;
 
-public class Duet {
+public class Template {
 
-    private static final String INPUT_FILE_NAME = "day18_input.txt";
+    private static final String INPUT_FILE_NAME = "dayXX_input.txt";
 
     public static void main(String[] args) throws Throwable {
         System.out.println("----   ADVENT Of code   2017    ----");
         long start = new Date().getTime();
         System.out.println("\n:::START = " + start);
-        System.out.println("\n                ---=== Day 18 ===---     ");
-        System.out.println("                   - Duet -     ");
-        System.out.println("\n          ---=== Part 1 ===---     ");
+        System.out.println("\n                ---=== Day XX ===---     ");
+        System.out.println("                   - XXXXXXXXX -     ");
+
+        System.out.println("\n    ---=== Part 1 ===---     ");
 
         loadInput(INPUT_FILE_NAME, "");
         partOne();
 
-        System.out.println("\n  Part 1 solution:   XXXXXX= [" + 1 + "]");
+        System.out.println("\n    Part 1 solution:   XXXXXX= [" );
 
 
         long p2Start = new Date().getTime();
-        System.out.println("\n  P1 Duration: " + (p2Start - start) + "ms (" + (p2Start - start) / 1000 + "s)");
+        System.out.println("\nP1 Duration: " + (p2Start - start) + "ms (" + (p2Start - start) / 1000 + "s)");
 
         System.out.println("=========================================================================================");
-        System.out.println("\n          ---=== Part 2 ===---     ");
 
         partTwo();
-        System.out.println("\n    Part 2 solution:   YYYYYYYYYYYY= [" + 2 + "]");
+        System.out.println("\n    Part 2 solution:   YYYYYYYYYYYY= [");
 
         closeInput();
 
 
         long end = new Date().getTime();
-        System.out.println("\n  P2 Duration: " + (end - p2Start) + "ms (" + (end - p2Start) / 1000 + "s)");
+        System.out.println("\nP2 Duration: " + (end - p2Start) + "ms (" + (end - p2Start) / 1000 + "s)");
         System.out.println("\nTotal Duration: " + (end - start) + "ms (" + (end - start) / 1000 + "s)");
+
         System.out.println("\n:::END = " + end);
     }
 
