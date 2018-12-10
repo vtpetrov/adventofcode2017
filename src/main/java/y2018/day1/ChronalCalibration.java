c5234072df89bba46771b0272b8acb228e1ff9f4
Veselin Petrov <veselin.petrov@urbanise.com>
2018-12-10 18:39:29 +0200

    Day 1, part 2

--- src/main/java/y2018/day1/ChronalCalibration.java
+++ src/main/java/y2018/day1/ChronalCalibration.java
@@ -1,14 +1,16 @@
 package y2018.day1;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
-import static helper.InputLoader.closeInput;
-import static helper.InputLoader.getMainIn;
-import static helper.InputLoader.loadInput;
+import static helper.InputLoader.*;
 
 public class ChronalCalibration {
 
     private static final String INPUT_FILE_NAME = "year_2018/day1_input.txt";
+    private static ArrayList<Integer> moves = new ArrayList<>();
 
     public static void main(String[] args) throws Throwable {
         System.out.println("----   ADVENT Of code   2018    ----");
@@ -20,6 +22,17 @@ public class ChronalCalibration {
         System.out.println("\n    ---=== Part 1 ===---     ");
 
         loadInput(INPUT_FILE_NAME, "");
+
+
+        //read and store the input so it will be used for both part 1 and 2:
+        while (getMainIn().hasNextLine()){
+
+            moves.add(Integer.parseInt(getMainIn().nextLine()));
+
+        }
+        closeInput();
+
+
         partOne();
 
 
@@ -29,9 +42,7 @@ public class ChronalCalibration {
         System.out.println("=========================================================================================");
 
         partTwo();
-        System.out.println("\n    Part 2 solution:   YYYYYYYYYYYY= [");
 
-        closeInput();
 
         long end = new Date().getTime();
         System.out.println("\nP2 Duration: " + (end - p2Start) + "ms (" + (end - p2Start) / 1000 + "s)");
@@ -44,13 +55,9 @@ public class ChronalCalibration {
     private static void partOne() {
 
         long freq = 0;
-        int move = 0;
 
-        while (getMainIn().hasNextLine()){
-
-            move = Integer.parseInt(getMainIn().nextLine());
+        for(Integer move : moves){
             freq += move;
-
         }
 
         System.out.println("\n    Part 1 solution:   resulting frequency= " + freq);
@@ -58,6 +65,24 @@ public class ChronalCalibration {
     }
 
     private static void partTwo() {
+
+        long freq = 0;
+        Set<Long> previousFreqs = new HashSet<>();
+        int loop = 1;
+
+        while (true) {
+            System.out.println("loop = " + loop);
+            for(Integer move : moves){
+                freq += move;
+
+                if(!previousFreqs.add(freq)){
+                    System.out.println("\n    Part 2 solution:   the first frequency reached twice is = " + freq);
+                    return;
+                }
+            }
+            loop++;
+        }
+
 
     }
 
